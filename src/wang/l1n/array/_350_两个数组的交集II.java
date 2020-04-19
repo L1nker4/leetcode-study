ec3ca6de81884f0a074c649ce3c2aa1be7d20749
l1nker4 <l1nker4@outlook.com>
2020-04-19 14:32:57 +0800

    day3

--- src/wang/l1n/array/_350_两个数组的交集II.java
+++ src/wang/l1n/array/_350_两个数组的交集II.java
@@ -16,42 +16,43 @@ public class _350_两个数组的交集II {
     /**
      * HashMap记录其中一个数组元素出现次数键值对
      * 再遍历另一个数组，判断是否在其中
+     *
      * @param nums1
      * @param nums2
      * @return
      */
     public int[] intersect1(int[] nums1, int[] nums2) {
 
-        int len1 = nums1.length,len2 = nums2.length,count = 0;
+        int len1 = nums1.length, len2 = nums2.length, count = 0;
 
         int[] res = new int[len1];
 
-        if(len1 == 0 || len2 == 0){
+        if (len1 == 0 || len2 == 0) {
             return new int[0];
         }
 
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int num : nums1){
-            if(map.containsKey(num)){
-                map.put(num,map.get(num)+1);
-            }else{
-                map.put(num,1);
+        for (int num : nums1) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
             }
         }
 
-        for(int num : nums2){
-            if(map.containsKey(num)){
-                if(map.get(num) == 0){
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                if (map.get(num) == 0) {
                     continue;
-                }else{
+                } else {
                     res[count++] = num;
-                    map.put(num,map.get(num)-1);
+                    map.put(num, map.get(num) - 1);
                 }
             }
         }
 
-        return Arrays.copyOfRange(res,0,count);
+        return Arrays.copyOfRange(res, 0, count);
     }
 
     public int[] intersect2(int[] nums1, int[] nums2) {
---
+++ src/wang/l1n/string/_125_验证回文串.java
@@ -0,0 +1,23 @@
+package wang.l1n.string;
+
+/**
+ * @author ：L1nker4
+ * @date ： 创建于  2020/4/19 12:45
+ * @description： https://leetcode-cn.com/problems/valid-palindrome/
+ */
+public class _125_验证回文串 {
+
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        char[] arr = s.toLowerCase().toCharArray();
+        int start = 0,end = arr.length -1;
+        while(start < end){
+            if (arr[start] != arr[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
