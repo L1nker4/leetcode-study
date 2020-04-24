5259d1cf946387d64f8949b7f9eb6d8ae760a1c1
l1nker4 <l1nker4@outlook.com>
2020-04-24 22:39:52 +0800

    day7

---
+++ src/wang/l1n/list/_142_环形链表II.java
@@ -0,0 +1,13 @@
+package wang.l1n.list;
+
+/**
+ * @author ：L1nker4
+ * @date ： 创建于  2020/4/24 22:35
+ * @description： https://leetcode-cn.com/problems/linked-list-cycle-ii/
+ */
+public class _142_环形链表II {
+
+    public ListNode detectCycle(ListNode head) {
+        return null;
+    }
+}
