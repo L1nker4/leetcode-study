42fce0643ab923ebee3bb25d81f2c743ce4ef4ce
l1nker4 <l1nker4@outlook.com>
2020-04-26 18:45:36 +0800

    day9

---
+++ src/wang/l1n/_70_爬楼梯.java
@@ -0,0 +1,18 @@
+package wang.l1n;
+
+/**
+ * @author ：L1nker4
+ * @date ： 创建于  2020/4/26 16:23
+ * @description： https://leetcode-cn.com/problems/climbing-stairs/
+ */
+public class _70_爬楼梯 {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
