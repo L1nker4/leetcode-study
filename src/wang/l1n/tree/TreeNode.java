11e1897ee4263de28ce03500a283ea577c19ace4
l1nker4 <l1nker4@outlook.com>
2020-05-06 20:28:48 +0800

    day14

---
+++ src/wang/l1n/tree/TreeNode.java
@@ -0,0 +1,11 @@
+package wang.l1n.tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/wang/l1n/tree/_98_验证二叉搜索树.java
@@ -0,0 +1,56 @@
+package wang.l1n.tree;
+
+import java.util.Stack;
+
+/**
+ * @author ：L1nker4
+ * @date ： 创建于  2020/5/5 21:24
+ * @description： https://leetcode-cn.com/problems/validate-binary-search-tree/
+ */
+public class _98_验证二叉搜索树 {
+    public boolean helper(TreeNode node, Integer lower, Integer upper) {
+        if (node == null) {
+            return true;
+        }
+
+        int val = node.val;
+        if (lower != null && val <= lower) {
+            return false;
+        }
+        if (upper != null && val >= upper) {
+            return false;
+        }
+
+        if (! helper(node.right, val, upper)) {
+            return false;
+        }
+        if (! helper(node.left, lower, val)) return false;
+        return true;
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+
+    public boolean isValidBST2(TreeNode root) {
+        Stack<TreeNode> stack = new Stack();
+        double inorder = - Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            // 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
+            if (root.val <= inorder) {
+                return false;
+            }
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+
+
+}
