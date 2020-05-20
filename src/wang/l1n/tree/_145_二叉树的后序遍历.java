c2c22f5b2b83a7ed3e63ec24021f7107bad8fe75
l1nker4 <l1nker4@outlook.com>
2020-05-20 15:06:07 +0800

    day19

---
+++ src/wang/l1n/tree/_145_二叉树的后序遍历.java
@@ -0,0 +1,17 @@
+package wang.l1n.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ：L1nker4
+ * @date ： 创建于  2020/5/20 14:10
+ * @description： https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
+ */
+public class _145_二叉树的后序遍历 {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> data = new ArrayList<>();
+        return data;
+    }
+}
