02203fcd5fac07fb13efde2b0a02ad6573f8d368
l1nker4 <l1nker4@outlook.com>
2020-06-01 18:00:02 +0800

    day21

--- src/wang/l1n/tree/_104_二叉树的最大深度.java
+++ src/wang/l1n/tree/_104_二叉树的最大深度.java
@@ -1,5 +1,9 @@
 package wang.l1n.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @author ：L1nker4
  * @date ： 创建于  2020/5/21 10:53
@@ -15,5 +19,29 @@ public class _104_二叉树的最大深度 {
             int right = maxDepth(root.right);
             return Math.max(left, right) + 1;
         }
+    }
+
+    public int maxDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int maxDepth = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            maxDepth++;
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = queue.poll();
+                if(temp.left != null){
+                    queue.add(temp.left);
+                }
+                if (temp.right != null){
+                    queue.add(temp.right);
+                }
+            }
+        }
+        return maxDepth;
     }
 }
